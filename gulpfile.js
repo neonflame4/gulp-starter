fbcd029bcc09b9d30bf99f382154ecdfca9960ef
Mark Nagengast <mark@neonfla.me>
2020-03-10 23:28:07 -0400

    update template

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 const { task, watch, src, dest, series } = require("gulp"),
-    autoprefixer = require("autoprefixer")
+    autoprefixer = require("gulp-autoprefixer")
     clean = require("gulp-clean"),
     sass = require("gulp-sass"),
-    sourcemaps = require("gulp-sourcemaps")
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create()
 ;
 
 const SRC_PATH = './src',
       DEST_PATH = './dist';
 
-const DIR_INPUT_SCSS = SRC_PATH + '/scss/**/*.scss';
+const DIR_INPUT_SCSS = SRC_PATH + '/assets/scss/**/*.scss';
+const DIR_INPUT_HTML =  [SRC_PATH + '/**/*.html', '!'+SRC_PATH+'/assets'];
       
 
-const DIR_OUTPUT_CSS = DEST_PATH + '/css';
+const DIR_OUTPUT_CSS = DEST_PATH + '/assets/css';
 
 
+
+/* ****************** SCSS TASKS ******************* */
 task('scss:clean', () => {
-    return src(DEST_PATH + "/**/*.*", {read: false})
+    return src(DIR_OUTPUT_CSS, {read: false, allowEmpty: true})
         .pipe(clean());
 });
     
@@ -27,6 +31,9 @@ task('scss:compile', () => {
                 sourceMap: true
             })
             .on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['cover 99.5% in US']
+        }))
         .pipe(sourcemaps.write('./maps'))
         .pipe(dest(DIR_OUTPUT_CSS));
 });
@@ -36,15 +43,40 @@ task('scss', series('scss:clean', 'scss:compile'));
 
 
 
+/* ****************** HTML TASKS ******************* */
+task('html:clean', () => {
+    return src(DIR_INPUT_HTML, {read: false, allowEmpty: true})
+        .pipe(clean());
+});
 
-
-
-task('watch', (cb) => {
-    series('scss');
-    watch( DIR_INPUT_SCSS )
-        .on('change', series('scss'))
-        .on('ready', series('scss'));
+task('html:copy', () => {
+    return src(DIR_INPUT_HTML)
+        .pipe(dest(DEST_PATH));
 });
 
 
-task('build', series('scss'));
+
+
+/* ****************** BUILD TASKS ****************** */
+task('serve', (cb) => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+    cb();
+});
+
+task('watch', (cb) => {
+    watch( DIR_INPUT_SCSS )
+        .on('ready', series('scss'))
+        .on('change', series('scss', browserSync.reload));
+
+    watch( DIR_INPUT_HTML )
+        .on('ready', series('html:clean','html:copy'))
+        .on('change', series('html:copy', browserSync.reload));
+
+    series('serve')();
+});
+
+task('build', series('html:copy','scss'));
