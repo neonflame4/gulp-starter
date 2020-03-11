f2507144c242351678d7ce5ea872bd68379ced16
Mark Nagengast <mnagengast@creditacceptance.com>
2020-03-11 14:08:04 -0400

    add flat file processing via panini

--- README.md
+++ README.md
@@ -20,4 +20,14 @@ npm install
 
 **Run your project:**
 
-To run the project, simply type in `gulp watch`. 
+To run the project, simply type in `gulp watch`. Whenever you make changes while using the file watcher, the browser will automatically refresh the page to preview your changes.
+
+**Generating Layouts with Components**
+
+Layouts are generated using panini (https://github.com/foundation/panini).  You can find more configuration options on their GitHub page.
+
+Files saved into the src/layouts folder can be used as site skeletons or design templates.
+
+Files saved to the src/pages folder make up the individual pages on your website.  Each of these files will be generated using the chosen layout template.
+
+Files saved to the src/partials folder are shared components that you can pull into pages and layout HTML files.
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const { task, watch, src, dest, series, parallel } = require("gulp"),
     sass = require("gulp-sass"),
     concat = require("gulp-concat"),
     minify = require("gulp-minify"),
+    panini = require("panini"),
     sourcemaps = require("gulp-sourcemaps"),
     browserSync = require("browser-sync").create()
 ;
@@ -19,7 +20,8 @@ const
 
 const 
     DIR_INPUT_FONTS = SRC_PATH + '/assets/fonts/**/*.scss',
-    DIR_INPUT_HTML =  [SRC_PATH + '/**/*.html', '!'+SRC_PATH+'/assets'],
+    DIR_INPUT_HTML =  [SRC_PATH + '/**/*.{htm,html,xhtml,php}', '!'+SRC_PATH + '/assets/**/*', '!'+SRC_PATH + '/templates/**/*'],
+    DIR_INPUT_HTML_TEMPLATES = SRC_PATH + '/templates';
     DIR_INPUT_IMAGES = SRC_PATH + '/assets/images/**/*',
     DIR_INPUT_JS = [SRC_PATH + '/assets/js/**/*', '!' + SRC_PATH + '/assets/js/vendor/**/*.js'],
     DIR_INPUT_VENDOR_JS = SRC_PATH + '/assets/js/vendor/**/*.js',
@@ -37,9 +39,13 @@ function getOutputPath() {
     return isBuild ? DIST_PATH : PUBLIC_PATH;
 }
 
+function paniniRefresh(done) {
+    panini.refresh();
+    done();
+}
 
 task('dest_clean', () => {
-    return src(getOutputPath(), {read: false, allowEmpty: true})
+    return src([DIST_PATH, PUBLIC_PATH], {read: false, allowEmpty: true})
         .pipe(clean());
 });
 
@@ -91,6 +97,16 @@ task('scss', series('scss:clean', 'scss:compile'));
 /* ****************** HTML TASKS ******************* */
 task('html:copy', () => {
     return src(DIR_INPUT_HTML)
+        .pipe(dest(getOutputPath()));
+});
+
+task('html:generate', () => {
+    return src(DIR_INPUT_HTML_TEMPLATES + '/pages/**/*.html')
+        .pipe(panini({
+            root:      DIR_INPUT_HTML_TEMPLATES + '/pages/',
+            layouts:   DIR_INPUT_HTML_TEMPLATES + '/layouts/',
+            partials:  DIR_INPUT_HTML_TEMPLATES + '/partials/'
+        }))
         .pipe(dest(getOutputPath()));
 });
 
@@ -148,22 +164,27 @@ task('serve', (cb) => {
     browserSync.init({
         server: {
             baseDir: "./public/",
-            injectChanges: true
-        }
+            injectChanges: true,
+        },
     });
     cb();
 });
 
 task('watch', (c1) => {
-    series('dest_clean')();
+    series('dest_clean','html:generate','fonts:copy','serve')();
 
     watch( DIR_INPUT_SCSS )
         .on('ready', series('scss:compile'))
         .on('change', series('scss:compile'));
 
-    watch( DIR_INPUT_HTML )
-        .on('ready', series('html:copy'))
-        .on('change', series('html:copy', browserSync.reload));
+    /* TO USE index.html files in root dir, use this */
+    // watch( DIR_INPUT_HTML )
+    //     .on('ready', series('html:copy'))
+    //     .on('change', series('html:copy', browserSync.reload));
+
+    /* TO USE constructed .html files, use this: */
+    watch( DIR_INPUT_HTML_TEMPLATES + '/**/*.html' )
+        .on('all', series(paniniRefresh, 'html:generate', browserSync.reload));
 
     watch( DIR_INPUT_JS )
         .on('ready', series('js'))
@@ -172,15 +193,15 @@ task('watch', (c1) => {
     watch( DIR_INPUT_IMAGES )
         .on('ready', series('images:copy'))
         .on('change', series('images:copy'));
-
-    series('fonts:copy','serve')();
 });
+
+task('clean:all', series('dest_clean'));
 
 task('build', c1 => {
     isBuild = true;
     
     series('dest_clean', c2 => {
-        parallel('html:copy','scss:build','js:build','images:copy','fonts:copy')();
+        parallel('html:copy','html:generate','scss:build','js:build','images:copy','fonts:copy')();
         c2();
     })();
     
