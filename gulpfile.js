8dc0c803b130d7cd98ec56217506065e21c8c3fb
Mark Nagengast <mark@neonfla.me>
2020-03-31 12:39:57 -0400

    cleanup gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,28 @@ const { task, watch, src, dest, series, parallel } = require( 'gulp' ),
       babel                                        = require( 'gulp-babel' ),
       panini                                       = require( 'panini' ),
       sourcemaps                                   = require( 'gulp-sourcemaps' ),
+      imageResize                                  = require( 'gulp-image-resize' ),
       browserSync                                  = require( 'browser-sync' ).create()
 ;
 
 
-let isBuild     = false;
-const
-    SRC_PATH    = './src',
-    DIST_PATH   = './dist',
-    PUBLIC_PATH = './public'
-;
+let isBuild  = false;
+const config = {
+  image_max_width: 1024,
+  image_max_height: 1024,
+  thumbnail_max_width: 200,
+  thumbnail_max_width: 200,
+};
 
 
 const
+    /* ROOT DIRECTORIES */
+    SRC_PATH                 = './src',
+    DIST_PATH                = './dist',
+    PUBLIC_PATH              = './public',
+    
+    
+    /* INPUT DIRECTORIES */
     DIR_INPUT_FONTS          = SRC_PATH + '/assets/fonts/**/*.scss',
     DIR_INPUT_HTML           = [
       SRC_PATH + '/**/*.{htm,html,xhtml,php}',
@@ -29,22 +38,26 @@ const
     ],
     DIR_INPUT_HTML_TEMPLATES = SRC_PATH + '/templates',
     DIR_INPUT_IMAGES         = SRC_PATH + '/assets/images/**/*',
+    DIR_INPUT_PROCESS_IMAGES = SRC_PATH + '/assets/images/gallery',
     DIR_INPUT_JS             = [
       SRC_PATH + '/assets/js/**/*',
       '!' + SRC_PATH + '/assets/js/vendor/**/*.js',
     ],
     DIR_INPUT_VENDOR_JS      = SRC_PATH + '/assets/js/vendor/**/*.js',
     DIR_INPUT_SCSS           = SRC_PATH + '/assets/scss/**/*.scss',
-    DIR_INPUT_COPYTEXT       = SRC_PATH + '/data'
+    DIR_INPUT_COPYTEXT       = SRC_PATH + '/data',
+    
+    
+    /* OUTPUT DIRECTORIES */
+    DIR_OUTPUT_CSS           = function () { return getOutputPath() + '/assets/css';},
+    DIR_OUTPUT_FONTS         = function () { return getOutputPath() + '/assets/fonts';},
+    DIR_OUTPUT_IMAGES        = function () { return getOutputPath() + '/assets/images';},
+    DIR_OUTPUT_JS            = function () { return getOutputPath() + '/assets/js';}
 ;
 
-const
-    DIR_OUTPUT_CSS    = function () { return getOutputPath() + '/assets/css';},
-    DIR_OUTPUT_FONTS  = function () { return getOutputPath() + '/assets/fonts';},
-    DIR_OUTPUT_IMAGES = function () { return getOutputPath() + '/assets/images';},
-    DIR_OUTPUT_JS     = function () { return getOutputPath() + '/assets/js';}
-;
 
+
+/* ****************** HELPER METHODS ******************* */
 function getOutputPath() {
   return isBuild ? DIST_PATH : PUBLIC_PATH;
 }
@@ -54,6 +67,10 @@ function paniniRefresh( done ) {
   done();
 }
 
+
+
+
+/* ****************** MAINTENANCE TASKS ******************* */
 task( 'clean', () => {
   return src( [DIST_PATH, PUBLIC_PATH], { read: false, allowEmpty: true } )
       .pipe( clean() );
@@ -63,11 +80,6 @@ task( 'clean', () => {
 
 
 /* ****************** SCSS TASKS ******************* */
-task( 'scss:clean', () => {
-  return src( DIR_OUTPUT_CSS(), { read: false, allowEmpty: true } )
-      .pipe( clean() );
-} );
-
 task( 'scss:compile', () => {
   return src( DIR_INPUT_SCSS )
       .pipe( sourcemaps.init() )
@@ -99,23 +111,16 @@ task( 'scss:build', () => {
       .pipe( dest( DIR_OUTPUT_CSS() ) );
 } );
 
-task( 'scss', series( 'scss:clean', 'scss:compile' ) );
-
-
 
 
 /* ****************** HTML TASKS ******************* */
-task( 'html:copy', () => {
-  return src( DIR_INPUT_HTML )
-      .pipe( dest( getOutputPath() ) );
-} );
-
 task( 'html:generate', () => {
   return src( DIR_INPUT_HTML_TEMPLATES + '/pages/**/*.html' )
       .pipe( panini( {
         root: DIR_INPUT_HTML_TEMPLATES + '/pages/',
         layouts: DIR_INPUT_HTML_TEMPLATES + '/layouts/',
         partials: DIR_INPUT_HTML_TEMPLATES + '/partials/',
+        helpers: SRC_PATH + '/helpers/',
         data: DIR_INPUT_COPYTEXT,
       } ) )
       .pipe( dest( getOutputPath() ) );
@@ -170,6 +175,18 @@ task( 'images:copy', () => {
 } );
 
 
+task( 'images:process', () => {
+  return src( DIR_INPUT_PROCESS_IMAGES + '**/*' )
+      .pipe( imageResize( {
+        quality: 0.5,
+        height: config.image_max_height,
+        width: config.image_max_width,
+      } ) )
+      .pipe( browserSync.stream() )
+      .pipe( dest( DIR_OUTPUT_IMAGES() ) );
+} );
+
+
 
 
 /* ****************** FONT TASKS ******************* */
@@ -182,24 +199,23 @@ task( 'fonts:copy', () => {
 
 
 /* ****************** BUILD TASKS ****************** */
-task( 'serve', ( cb ) => {
-  browserSync.init( {
+task( 'serve', () => {
+  return browserSync.init( {
     server: {
       baseDir: './public/',
       injectChanges: true,
     },
   } );
-  cb();
 } );
 
 task( 'watch', ( c1 ) => {
-  series( 'clean', 'html:generate', 'fonts:copy', 'serve' )();
+  series( 'clean', parallel( 'html:generate', 'fonts:copy' ), 'serve' )();
   
   watch( DIR_INPUT_SCSS )
       .on( 'ready', series( 'scss:compile' ) )
       .on( 'change', series( 'scss:compile' ) );
   
-  /* TO USE constructed .html files, use this: */
+  
   watch( [
     DIR_INPUT_HTML_TEMPLATES + '/**/*.html',
     DIR_INPUT_COPYTEXT + '/**/*',
@@ -215,15 +231,13 @@ task( 'watch', ( c1 ) => {
       .on( 'change', series( 'images:copy' ) );
 } );
 
-task( 'clean:all', series( 'clean' ) );
 
-task( 'build', c1 => {
+task( 'build', done => {
   isBuild = true;
+  series(
+      'clean',
+      parallel( 'html:generate', 'scss:build', 'js:vendor', 'js:build', 'images:process', 'fonts:copy' ),
+  )();
   
-  series( 'clean', c2 => {
-    parallel( 'html:copy', 'html:generate', 'scss:build', 'js:build', 'images:copy', 'fonts:copy' )();
-    c2();
-  } )();
-  
-  c1();
+  done();
 } );
